a00dabf62cba53170ac4cd3f31eac00701f98b5a
Akaehomen peace <peacedavid049@gmail.com>
2023-11-04 23:49:38 +0100

    commit

---
+++ script.js
@@ -0,0 +1,72 @@
+const closeBtn = document.getElementById("close-btn");
+const hamburger = document.getElementById("hamburger");
+const navigation = document.getElementById("navigation");
+const submitBtn = document.getElementById("submit-btn");
+const emailInput = document.getElementById("emailInput");
+const err = document.getElementById("error-icon");
+const errorMsg = document.getElementById("errorMsg");
+const commentContainer = document.querySelectorAll("#question");
+const answer = document.querySelectorAll(".answer");
+const bookmarking = document.getElementById("bookmarking");
+const sharing = document.getElementById("sharing");
+const speddy = document.getElementById("speddy");
+const sectionThree = document.getElementById("section-three");
+const sectionTwo = document.getElementById("section-two");
+const sectionOne = document.getElementById("section-one");
+const sectionThreeImg = document.getElementById("section-three-img");
+const sectionTwoImg = document.getElementById("section-two-img");
+const sectionOneImg = document.getElementById("section-one-img");
+const arrow = document.querySelectorAll("#arrow");
+
+
+submitBtn.addEventListener("click", () => {
+
+  const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+
+  if(!emailPattern.test(emailInput.value.trim())){
+    err.style.display = "inline-flex";
+    errorMsg.style.display = "block";
+    errorMsg.innerText = "Whoops, make sure it's an email";
+    emailInput.style.borderColor = "#fa5757";
+  }else {
+    err.style.display = "none";
+    errorMsg.style.display = "none";
+    emailInput.style.borderColor = "transparent";
+  }
+})
+
+speddy.addEventListener("click", () => {
+  sectionTwo.style.display = "block";
+  sectionOne.style.display = "none";
+  sectionThree.style.display = "none";
+  sectionTwoImg.style.display = "block";
+  sectionOneImg.style.display = "none";
+  sectionThreeImg.style.display = "none";
+  speddy.style.borderBottomColor = "#fa5757";
+  bookmarking.style.borderBottomColor = "transparent";
+  sharing.style.borderBottomColor = "transparent";
+})
+
+bookmarking.addEventListener("click", () => {
+  sectionTwo.style.display = "none";
+  sectionOne.style.display = "block";
+  sectionThree.style.display = "none";
+  sectionTwoImg.style.display = "none";
+  sectionOneImg.style.display = "block";
+  sectionThreeImg.style.display = "none";
+  speddy.style.borderBottomColor = "transparent";
+  bookmarking.style.borderBottomColor = "#fa5757";
+  sharing.style.borderBottomColor = "transparent";
+})
+
+sharing.addEventListener("click", () => {
+  sectionTwo.style.display = "none";
+  sectionOne.style.display = "none";
+  sectionThree.style.display = "block";
+  sectionTwoImg.style.display = "none";
+  sectionOneImg.style.display = "none";
+  sectionThreeImg.style.display = "block";
+  speddy.style.borderBottomColor = "transparent";
+  bookmarking.style.borderBottomColor = "transparent";
+  sharing.style.borderBottomColor = "#fa5757";
+})
